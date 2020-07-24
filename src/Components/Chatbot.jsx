705db9a2e2fde6a05172e5299cae68d393e6ab6f
Prem Kumar <pr775390@dal.ca>
2020-07-23 21:59:30 -0300

    Created prem branch for chatbot and messaging service

---
+++ src/Components/Chatbot.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Chatbot extends Component{
+
+    render(){
+        return(
+            <div>
+                <p>this is for Chatbot</p>
+            </div>
+        )
+    }
+}
+
+export default Chatbot
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Chatbot from './Components/Chatbot'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Chatbot />
   </React.StrictMode>,
   document.getElementById('root')
 );
